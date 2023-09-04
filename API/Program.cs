31596eac095c1c05cffeb3e98866cfeaae6e3829
Ayushab35 <ayush.bhandarkar@spit.ac.in>
2023-09-04 13:32:57 +0530

    Migrations Done

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-
+using Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<DbContext>(opt =>{
+builder.Services.AddDbContext<DataContext>(opt =>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
